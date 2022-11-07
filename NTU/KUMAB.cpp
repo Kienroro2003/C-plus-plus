6c2309d9fbda274f4cad65890d71696dd258b824
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2022-11-07 12:04:42 +0700

    udpate:

---
+++ NTU/KUMAB.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+   freopen ("input.txt", "r", stdin);
+   freopen ("output.txt", "w", stdout);
+#endif
+   int a,b,c,d;
+   cin >> a;
+    b = a / 10;
+   do{
+       c = a + 3 * b;
+       d = b;
+       b = c / 10;
+   } while (d != b);
+   cout << b * 3 + a;
+   return 0;
+}
---
+++ algorithm/algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by Kienroro on 08/10/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
